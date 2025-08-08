3219910f899761acf14d8d6636cfa21005a7436c
muvinio <moo0v@mail.ru>
2025-08-08 16:21:28 +0400

    Исправлена шапка

--- price-checker-front/src/components/header.tsx
+++ price-checker-front/src/components/header.tsx
@@ -5,40 +5,15 @@ import './css/header.css'; // подключи стили, если есть
 export default function Header({ isAuth, onLogout }: { isAuth: boolean, onLogout: () => void }) {
   return (
     <header className="header">
-      <span style={{ flex: 1 }}>Price Checker </span>
-      {isAuth ? (
-        <button
-          onClick={onLogout}
-          style={{
-            background: '#4caf50',
-            color: '#fff',
-            border: 'none',
-            borderRadius: '6px',
-            padding: '8px 16px',
-            fontWeight: 'bold',
-            cursor: 'pointer',
-            marginRight: '100px'
-          }}
-        >
-          Выйти
-        </button>
-      ) : (
-        <button
-          onClick={() => window.location.reload()}
-          style={{
-            background: '#2196f3',
-            color: '#fff',
-            border: 'none',
-            borderRadius: '6px',
-            padding: '8px 16px',
-            fontWeight: 'bold',
-            cursor: 'pointer',
-            marginRight: '100px'
-          }}
-        >
-          Войти в аккаунт
-        </button>
-      )}
+      <div className='header-inner'>
+        
+        <span className='name'>SkinWorth </span>
+        {isAuth ? (
+          <button onClick={onLogout} className='button'> Выйти </button>) : (
+          <button
+            onClick={() => window.location.reload()} className='button'> Войти в аккаунт </button>
+        )}
+      </div>
     </header>
   );
 }
