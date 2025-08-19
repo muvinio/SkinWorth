d5c94301d39b3a87cce5c2dd0e8392b90ab6083c
muvinio <moo0v@mail.ru>
2025-08-19 17:05:21 +0400

    поменяны порты

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   origin: 'http://localhost:3001', // или 3001, если ты его используешь
 });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
