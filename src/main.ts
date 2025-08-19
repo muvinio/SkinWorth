51daabe4927b635e1fe792e16cd68b1a46e9f65f
muvinio <moo0v@mail.ru>
2025-08-19 15:59:02 +0400

    все работает

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as session from 'express-session';
-import * as passport from 'passport';
+
 import { User } from '@prisma/client';
 
 async function bootstrap() {
@@ -15,19 +14,6 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  
-  app.use(
-    session({
-      secret: 'super_secret_key',
-      resave: false,
-      saveUninitialized: false,
-    }),
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  passport.serializeUser((user, done) => done(null, user));
-passport.deserializeUser((obj: false | User | null | undefined, done) => done(null, obj));
   app.enableCors({
   origin: 'http://localhost:3001', // или 3001, если ты его используешь
 });
